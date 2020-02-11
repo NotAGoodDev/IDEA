0f16a79a871ff5569402b5eb0c9df28eeeaff992
mdesantes <marco.desantes@globalincubator.com>
2020-02-11 12:59:17 +0100

    init

---
+++ src/main/java/ucm/gps/idea/IdeaApplication.java
@@ -0,0 +1,13 @@
+package ucm.gps.idea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ucm/gps/idea/IdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package ucm.gps.idea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
