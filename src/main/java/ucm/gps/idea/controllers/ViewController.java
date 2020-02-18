5fa22f0f37d10b62a98c86a7e4f257b3c719ad95
silverzero <marcodes@ucm.es>
2020-02-18 21:04:06 +0100

    views

---
+++ src/main/java/ucm/gps/idea/controllers/ViewController.java
@@ -0,0 +1,29 @@
+package ucm.gps.idea.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ViewController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ViewController.class);
+
+    @RequestMapping(value="/home", method = RequestMethod.GET )
+    public String home(){
+        return "/home.html";
+    }
+
+    @RequestMapping(value="/enterprise", method = RequestMethod.GET )
+    public String enterprise(){
+        return "/enterprise.html";
+    }
+
+    @RequestMapping(value="/creator", method = RequestMethod.GET )
+    public String creator(){
+        return "/creator.html";
+    }
+
+}
