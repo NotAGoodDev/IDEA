8afaf26917f87236d7246c0ffab50962d50a5ecc
Alvaro San Jose <alvsanjo@ucm.es>
2020-03-02 18:10:08 +0100

    Creator Service CRUD

--- src/main/java/ucm/gps/idea/services/CreatorService.java
+++ src/main/java/ucm/gps/idea/services/CreatorService.java
@@ -1,9 +1,14 @@
 package ucm.gps.idea.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ucm.gps.idea.entities.Creator;
+import ucm.gps.idea.entities.Enterprise;
 import ucm.gps.idea.repositories.CreatorRepository;
+
+import java.util.List;
 
 @Service
 public class CreatorService {
@@ -11,7 +16,21 @@ public class CreatorService {
     @Autowired
     CreatorRepository creatorRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(IdeaService.class);
+
+    public List<Creator> list() {
+        return creatorRepository.findAll();
+    }
+
+    public Creator index(Integer id) throws Exception {
+        return creatorRepository.findById(id).orElseThrow(Exception::new);
+    }
+
     public Creator create(Creator creator) { return creatorRepository.save(creator); }
+
+    public void delete(Integer id) {
+        creatorRepository.deleteById(id);
+    }
 
     //pendiente de picar
 }
