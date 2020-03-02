985360c4cca424ddec10a65400d969f776f3eead
silverzero <marcodes@ucm.es>
2020-03-02 09:50:21 +0100

    fix application

--- src/main/java/ucm/gps/idea/controllers/ViewController.java
+++ src/main/java/ucm/gps/idea/controllers/ViewController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("views")
+@Controller
 public class ViewController {
 
     private static final Logger logger = LoggerFactory.getLogger(ViewController.class);
