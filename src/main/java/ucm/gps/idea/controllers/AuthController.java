4b6629506c8b5c0c34ee8b9788c2ded7bd3d579f
diegoisar <diegosanfer99@gmail.com>
2020-03-02 15:23:53 +0100

    created AuthController

--- src/main/java/ucm/gps/idea/controllers/AuthController.java
+++ src/main/java/ucm/gps/idea/controllers/AuthController.java
@@ -1,9 +1,12 @@
 package ucm.gps.idea.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/auth")
 public class AuthController {
+
+
 }
