28bf94476bc6901b774c4e1b24c4d37151591fff
csegundo <carlossn20165@gmail.com>
2020-03-14 16:39:39 +0100

    Spring Security Enterprise

--- src/main/java/ucm/gps/idea/services/EnterpriseService.java
+++ src/main/java/ucm/gps/idea/services/EnterpriseService.java
@@ -3,14 +3,21 @@ package ucm.gps.idea.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ucm.gps.idea.entities.Enterprise;
 import ucm.gps.idea.repositories.EnterpriseRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class EnterpriseService {
+public class EnterpriseService implements UserDetailsService {
 
     @Autowired
     EnterpriseRepository enterpriseRepository;
@@ -35,5 +42,18 @@ public class EnterpriseService {
 
     public Enterprise findByCif(String cif){
         return enterpriseRepository.findByCIF(cif);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Enterprise ret = enterpriseRepository.findByUsername(username);
+
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority("USER"));
+        roles.add(new SimpleGrantedAuthority("ADMIN"));
+
+        UserDetails details = new User(ret.getUsername(), ret.getPassword(), roles);
+
+        return details;
     }
 }
--- src/test/java/ucm/gps/idea/IdeaApplicationTests.java
+++ src/test/java/ucm/gps/idea/IdeaApplicationTests.java
@@ -1,12 +1,15 @@
 package ucm.gps.idea;
 
 import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import ucm.gps.idea.entities.Creator;
+import ucm.gps.idea.entities.Enterprise;
 import ucm.gps.idea.repositories.CreatorRepository;
+import ucm.gps.idea.repositories.EnterpriseRepository;
 
 import java.sql.Date;
 
@@ -27,28 +30,53 @@ public class IdeaApplicationTests {
 	*/
 
 	@Autowired
-    private CreatorRepository repo;
+    private CreatorRepository repoCreator;
+	@Autowired
+    private EnterpriseRepository repoEnterprise;
 	@Autowired
     private BCryptPasswordEncoder encoder;
 
 	@Test
     public boolean registerCreatorTest(){
 	    Creator first = new Creator();
-	    first.setId(2);
-	    first.setName("Raul");
+	    first.setId(3);
+	    first.setName("Carlos");
         first.setLastName("Garcia Rodriguez");
         first.setBirthDate(new Date(System.currentTimeMillis())); // Fecha actual
         first.setTelephone("655889401");
         first.setAddress("Calle ventana 8");
-        first.setEmail("raul@gmail.es");
-        first.setUsername("raulon_md");
-        first.setPassword(encoder.encode("raul123"));
+        first.setEmail("carlos@gmail.es");
+        first.setUsername("carletes_80");
+        first.setPassword(encoder.encode("carlos123"));
         first.setActive(true);
 
-        Creator ret = repo.save(first);
+        Creator ret = repoCreator.save(first);
 
         assertEquals(ret.getPassword(), first.getPassword());
         //assertTrue(ret.getPassword().equalsIgnoreCase(first.getPassword()));
+        return true;
+    }
+
+    @Test
+    public boolean registerEnterpriseTest(){
+        Enterprise e = new Enterprise();
+
+        e.setId(2);
+        e.setName("Microsft");
+        e.setCIF("1234567890");
+        e.setAddress("Calle California");
+        e.setEmail("mcfst@md.cf");
+        e.setTelephone("912344435");
+        e.setCreditCard(123456789);
+        e.setUsername("MicrosoftMadrid");
+        e.setPassword(encoder.encode("micro123"));
+        e.setRemainingIdeas(10);
+        e.setActive(true);
+
+        Enterprise ret = repoEnterprise.save(e);
+
+        assertEquals(e.getPassword(), ret.getPassword());
+
         return true;
     }
 }
