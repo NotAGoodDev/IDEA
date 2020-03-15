57e484b20382dbb396704fd04fb4ae86d65114e3
Álvaro <alvort03@ucm.es>
2020-03-15 14:20:07 +0100

    Little changes, JS and HTML at register

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,24 +1,17 @@
 $(document).ready(function() {
     //Formulario de registro de creador
-    $( "#form-registro-creador" ).submit(function( event ) {
+    $( "#form-register" ).submit(function( event ) {
         event.preventDefault();
         var c = {};
 
-        var form = $( this ),
-        name = form.find( "input[name='name']" ).val(),
-        url = "auth/creator/register";
-        c.name = form.find( "input[name='name']" ).val();
-        c.lastName = form.find( "input[name='lastName']" ).val();
-        c.birthDate = form.find( "input[name='birthDate']" ).val();
-        c.telephone = form.find( "input[name='telephone']" ).val();
-        c.address = form.find( "input[name='address']" ).val();
-        c.email = form.find( "input[name='email']" ).val();
+        var form = $( this );
+        url = "auth/register";
         c.username = form.find( "input[name='username']" ).val();
         c.password = form.find( "input[name='password']" ).val();
-        c.active = true;
+        c.email = form.find( "input[name='email']" ).val();
+        c.roles = ["ROLE_ADMIN", "ROLE_USER"];
 
         ApiController.post(url, c, function (response) {
-
         })
     });
 })
