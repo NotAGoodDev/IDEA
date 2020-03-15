140cd6ecaaff34390c7241f8ab3af44d89042453
Álvaro <alvort03@ucm.es>
2020-03-15 14:44:55 +0100

    Formulario registro funcional

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
     //Formulario de registro de creador
     $( "#form-register" ).submit(function( event ) {
         event.preventDefault();
-        var c = {};
+        var params = {};
 
         var form = $( this );
-        url = "auth/register";
-        c.username = form.find( "input[name='username']" ).val();
-        c.password = form.find( "input[name='password']" ).val();
-        c.email = form.find( "input[name='email']" ).val();
-        c.roles = ["ROLE_ADMIN", "ROLE_USER"];
+        params.username = form.find( "input[name='username']" ).val();
+        params.password = form.find( "input[name='password']" ).val();
+        params.email = form.find( "input[name='email']" ).val();
+        params.type = "1";
+        params.roles = ["ROLE_ADMIN", "ROLE_USER"];
 
-        ApiController.post(url, c, function (response) {
+        ApiController.post("auth/register", params, function (response) {
         })
     });
 })
