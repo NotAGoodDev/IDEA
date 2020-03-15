7f06f13739bbf96a99ddae262de883dbc619953f
diegoisar <diegosanfer99@gmail.com>
2020-03-15 16:04:06 +0100

    Correccion

--- src/main/java/ucm/gps/idea/repositories/DealRepository.java
+++ src/main/java/ucm/gps/idea/repositories/DealRepository.java
@@ -1,8 +1,10 @@
 package ucm.gps.idea.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ucm.gps.idea.entities.Deal;
 
+@Repository
 public interface DealRepository extends JpaRepository<Deal,Integer> {
 }
 
