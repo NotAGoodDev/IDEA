cafc5f7b36c50563bab4da1060c140951d7d1008
Gerardo Meiro Mendoza <gerardo.meiro@clupik.com>
2020-03-17 12:48:01 +0100

    Perfil empresa, falta conectar con back,y cargar los fichero js con jquery

---
+++ src/main/resources/static/js/enterprise/index.js
@@ -0,0 +1,35 @@
+// Dadas la division que contiene todas las pestañas y la de la pestaña que se
+// quiere mostrar, la funcion oculta todas las pestañas a excepcion de esa.
+function cambiarPestanna(pestannas,pestanna) {
+
+    // Obtiene los elementos con los identificadores pasados.
+    pestanna = document.getElementById(pestanna.id);
+    listaPestannas = document.getElementById(pestannas.id);
+
+    // Obtiene las divisiones que tienen el contenido de las pestañas.
+    cpestanna = document.getElementById('c'+pestanna.id);
+    listacPestannas = document.getElementById('contenido'+pestannas.id);
+
+    i=0;
+    // Recorre la lista ocultando todas las pestañas y restaurando el fondo
+    // y el padding de las pestañas.
+    while (typeof listacPestannas.getElementsByTagName('div')[i] != 'undefined'){
+        $(document).ready(function(){
+            $(listacPestannas.getElementsByTagName('div')[i]).css('display','none');
+            $(listaPestannas.getElementsByTagName('li')[i]).css('background','');
+            $(listaPestannas.getElementsByTagName('li')[i]).css('padding-bottom','');
+        });
+        i += 1;
+    }
+
+    $(document).ready(function(){
+        // Muestra el contenido de la pestaña pasada como parametro a la funcion,
+        // cambia el color de la pestaña y aumenta el padding para que tape el
+        // borde superior del contenido que esta juesto debajo y se vea de este
+        // modo que esta seleccionada.
+        $(cpestanna).css('display','');
+        $(pestanna).css('background','dimgray');
+        $(pestanna).css('padding-bottom','2px');
+    });
+
+}
--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
+    jQuery.loadCSS = function(url) {
+        if (!$('link[href="' + url + '"]').length)
+            $('head').append('<link rel="stylesheet" type="text/css" href="' + url + '">');
+    }
     $(function () {
         $("header").empty();
         $("header").load('/header');
+
+
+        $.loadCSS('css/quienes-somos.css');
+
+
+        $.getScript('js/enterprise/index.js', function() {
+            alert('Load was performed.');
+        });
     });
+
 });
