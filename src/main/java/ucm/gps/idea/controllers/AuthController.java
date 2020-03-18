e7ede3105c91c2f9d96dd8e74fc1cb6aa640d809
Alvaro San Jose <alvsanjo@ucm.es>
2020-03-18 19:56:38 +0100

    Refactor de RegUser a ModelUser + Función modify actualizada (falta confirmar tema el principal)

--- src/main/java/ucm/gps/idea/controllers/AuthController.java
+++ src/main/java/ucm/gps/idea/controllers/AuthController.java
@@ -14,13 +14,13 @@ import ucm.gps.idea.entities.Creator;
 import ucm.gps.idea.entities.Enterprise;
 import ucm.gps.idea.entities.Role;
 import ucm.gps.idea.entities.User;
-import ucm.gps.idea.models.RegisterUser;
+import ucm.gps.idea.models.ModelUser;
 import ucm.gps.idea.services.CreatorService;
 import ucm.gps.idea.services.EnterpriseService;
 import ucm.gps.idea.services.RoleService;
 import ucm.gps.idea.services.UserService;
 
-import java.text.DateFormat;
+import java.security.Principal;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -44,7 +44,7 @@ public class AuthController {
     private BCryptPasswordEncoder encoder;
 
     @PostMapping("/register")
-    public ResponseEntity<User> create(@RequestBody RegisterUser regUser) {
+    public ResponseEntity<User> create(@RequestBody ModelUser regUser) {
         // NOTA: De front nos llega "Creador" o "Empresa"
 
         User user = null;
@@ -116,24 +116,38 @@ public class AuthController {
         }
     }
 
-    @PostMapping("/profile/modify")
-    public ResponseEntity<User> modify(@RequestBody RegisterUser Reguser){
+    @PutMapping("/profile/modify")
+    public ResponseEntity<User> modify(@RequestBody ModelUser moduser, Principal principal){
 
-        User user = userService.findByUsername(Reguser.getUsername());
-
-        //Utilizado create de creator/enterprise para guardar en la BBDD mediante el repositorio
+        User user = userService.findByUsername(principal.getName());
 
         if(user.getType().equalsIgnoreCase("Creador")){
             try{
+                user.setAddress(moduser.getAddress());
+                user.setEmail(moduser.getEmail());
+                user.setName(moduser.getName());
+                user.setTelephone(moduser.getTelephone());
+                userService.save(user);
+
                 Creator c = creatorService.index(user.getId());
+                c.setLastName(moduser.getLastName());
                 c = creatorService.create(c);
+
                 return new ResponseEntity<>(c, HttpStatus.OK);
             }catch (Exception e){ return new ResponseEntity<>(HttpStatus.FORBIDDEN); }
         }
         else if(user.getType().equalsIgnoreCase("Empresa")){
             try{
+                user.setAddress(moduser.getAddress());
+                user.setEmail(moduser.getEmail());
+                user.setName(moduser.getName());
+                user.setTelephone(moduser.getTelephone());
+                userService.save(user);
+
                 Enterprise e = enterpriseService.index(user.getId());
+                e.setCreditCard(moduser.getCardNumber());
                 e = enterpriseService.create(e);
+
                 return new ResponseEntity<>(e, HttpStatus.OK);
             }catch (Exception e){ return new ResponseEntity<>(HttpStatus.FORBIDDEN); }
         }
