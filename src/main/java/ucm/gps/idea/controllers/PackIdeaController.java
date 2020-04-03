39e01e8f58eee906df2814cf7ed8616a8fa46226
csegundo <carlossn20165@gmail.com>
2020-04-03 20:52:05 +0200

    Actualizado el js de buyPack y algo de la parte de back

--- src/main/java/ucm/gps/idea/controllers/PackIdeaController.java
+++ src/main/java/ucm/gps/idea/controllers/PackIdeaController.java
@@ -3,15 +3,23 @@ package ucm.gps.idea.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import ucm.gps.idea.entities.Enterprise;
 import ucm.gps.idea.entities.PackIdea;
+import ucm.gps.idea.entities.User;
 import ucm.gps.idea.models.ModelPackToBuy;
+import ucm.gps.idea.services.EnterpriseService;
 import ucm.gps.idea.services.PackIdeaService;
+import ucm.gps.idea.services.UserService;
 
+import java.security.Principal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/packages")
@@ -19,6 +27,9 @@ public class PackIdeaController {
 
     private static final Logger logger = LoggerFactory.getLogger(PackIdeaController.class);
     private  static final Double PRICE_PER_IDEA = 7.0;
+
+    @Autowired
+    UserService userService;
 
     @Autowired
     PackIdeaService packIdeaService;
@@ -29,14 +40,17 @@ public class PackIdeaController {
         return new ResponseEntity<>(packages, HttpStatus.OK);
     }
 
-    @PostMapping("/buy")
-    public ResponseEntity<?> create(@RequestBody ModelPackToBuy modelPackToBuy) {
-        // DATOS RELATIVOS A LA COMPRA
+    @PutMapping("/buy")
+    public ResponseEntity<Double> create(@RequestBody ModelPackToBuy modelPackToBuy, Principal principal) {
+        // Calculamos el total de la compra, añadimos las ideas a la empresa y devolvemos el total para q pague
         double total = modelPackToBuy.getNumIdeasToBuy() * PRICE_PER_IDEA;
         total -= total * (modelPackToBuy.getDiscount() / 100.0);
         logger.info(modelPackToBuy.toString() + " " + total);
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        User user = userService.findByUsername(principal.getName());
+        ((Enterprise)user).setRemainingIdeas(((Enterprise)user).getRemainingIdeas() + modelPackToBuy.getNumIdeasToBuy());
+
+        return new ResponseEntity<>(total, HttpStatus.OK);
     }
 
     @GetMapping("/getPack")
@@ -50,18 +64,15 @@ public class PackIdeaController {
     }
 
     @GetMapping("/getDiscount")
-    public ResponseEntity<?> getDiscount(@RequestBody Integer numIdeas){
-        // De prueba
-        Integer ret;
+    public ResponseEntity<Map<Pair<Integer, Integer>, Integer>> getDiscount(){ //@RequestBody Integer numIdeas){
+        Map<Pair<Integer, Integer>, Integer> discountTable = new HashMap<>();
 
-        if(numIdeas >= 1 && numIdeas < 40)
-            ret = 10;
-        else if (numIdeas >= 40 && numIdeas < 250)
-            ret = 25;
-        else
-            ret = 30;
+        discountTable.put(Pair.of(40, 250), 5);
+        discountTable.put(Pair.of(250, 2000), 10);
+        discountTable.put(Pair.of(2000, 3500), 15);
+        discountTable.put(Pair.of(3500, 5000), 20);
 
-        return new ResponseEntity<>(ret, HttpStatus.OK);
+        return new ResponseEntity<>(discountTable, HttpStatus.OK);
     }
 
     // En formato String JSON por si ayuda en front
--- src/main/resources/static/js/enterprise/packs.js
+++ src/main/resources/static/js/enterprise/packs.js
@@ -1,103 +1,96 @@
 $(document).ready(function() {
-    $("#form-packs").submit(function (event) {
-        event.preventDefault();
 
-        /* TODO PROBLEMAS:
-        *       - Hace la llamada bien a la primera, el segundo click hace 2 peticiones, al 3er click hace 3 y asi...
-        *       - No sale el alert cuando da fallo/suceso
-        */
+    let discounts; // Array donde se encuentran los descuentos segun el numero de ideas que se vayan a comprar
+    let purchaseData = {}; // Para coger los datos de los packs de ideas
+    let sesionData = {}; // Para coger los datos de la sesion
+    let payment = {}; // Para llamar a la pasarela de pago
 
-        let purchaseData = {};
+    // Hacemos esta llamada para coger los datos de la sesion para saber el nombre de la empresa, su tarjeta de credito...
+    ApiController.get("auth/session", "", false).then(function (data) {
+        sesionData = data;
+        purchaseData.cardNumber = data.creditCard;
+        purchaseData.ownerName = data.name;
+    });
 
-        $("#pack10ideas").click(function () {
-            let txt = document.getElementById("descuento-seleccion").value; // coger valor de la linea 73 del html
-            let numb = txt.match(/\d/g);
-            numb = numb.join("");
-            alert(numb);
+    // Hacemos esta llamada para no tener que hacer una peticion cada vez que se seleccione un numero de ideas
+    ApiController.get("packages/getDiscount", "", false).then(function (discountTable) {
+        discounts = Object.entries(discountTable); // Array con cada posicion: [PAIR INTEGER], el PAIR en string.
+    });
 
-            /*purchaseData.numIdeasToBuy = 10;
-            purchaseData.discount = 0;
-            purchaseData.cardNumber = 123;
-            purchaseData.ownerName = "Pack10Ideas";
-            purchaseData.validateNumber = 456;
-            purchaseData.expirationDate = "2024-04-05";
+    // Esto es solo para mostrarselo a front
+    $("#numIdeas").on('input',function () {
+        if(discounts != null){
+            let numIdeasToBuy = document.getElementById("numIdeas").value;
+            let eachItem, disc, pricePerIdea = 7, total, i, str;
 
-            ApiController.post("packages/buy", purchaseData).then(function (data) {
-                if(data){
-                    alert("Pago realizado con exito.");
-                    window.location.href = "/home"
-                }else{
-                    alert("Error al hacer el pago.");
+            for (i = 0; i < discounts.length; ++i){
+                // Hacemos esto para quedarnos solo con los numero del string ==> ["PAIR", INTEGER] = [ideasRango, dto]
+                eachItem = discounts[i];
+                str = eachItem[0];
+                str = str.replace('Pair(first=', '');
+                str = str.replace('second=', '');
+                str = str.replace(')', '');
+                str = str.split(',');
+                disc = eachItem[1];
+
+                if(parseInt(numIdeasToBuy, 10) >= parseInt(str[0], 10) &&
+                    parseInt(numIdeasToBuy, 10) < parseInt(str[1], 10)){
+
+                    purchaseData.discount = disc; // Descuento para aplicarlo despues a la compra
+                    total = pricePerIdea * numIdeasToBuy;
+                    total -= total * (disc / 100);
+                    document.getElementById("descuento-seleccion").innerHTML = "Descuento: " + disc + "%";
+                    document.getElementById("descuento-precio").innerHTML = total.toFixed(2) + " €";
                 }
-            });*/
-        });
+            }
+        }
+    });
 
-        $("#packXideas").click(function () {
-            purchaseData.numIdeasToBuy = document.getElementById("numIdeas").value;
-            purchaseData.discount = 100;
-            purchaseData.cardNumber = 789;
-            purchaseData.ownerName = "PackXIdeas";
-            purchaseData.validateNumber = 1011;
-            purchaseData.expirationDate = "2024-04-05";
+    $("#pack10ideas").click(function () {
+        purchaseData.numIdeasToBuy = 10;
+        purchaseData.discount = 0;
+        purchaseData.validateNumber = 456;
+        purchaseData.expirationDate = "2024-04-05";
 
-            ApiController.post("packages/buy", purchaseData).then(function (data) {
-                if(data){
-                    alert("Pago realizado con exito.");
-                    window.location.href = "/home"
-                }else{
-                    alert("Error al hacer el pago.");
-                }
-            });
-        });
-
-        $("#pack20ideas").click(function () {
-            purchaseData.numIdeasToBuy = 20;
-            purchaseData.discount = 0;
-            purchaseData.cardNumber = 1213;
-            purchaseData.ownerName = "Pack20Ideas";
-            purchaseData.validateNumber = 1415;
-            purchaseData.expirationDate = "2024-04-05";
-
-            ApiController.post("packages/buy", purchaseData).then(function (data) {
-                if(data){
-                    alert("Pago realizado con exito.");
-                    window.location.href = "/home"
-                }else{
-                    alert("Error al hacer el pago.");
-                }
+        ApiController.put("packages/buy", purchaseData, false).then(function (data) {
+            payment.description = "Compra de un pack de 10 ideas";
+            payment.amount = data;
+            payment.currency = "EUR"; // En back es un enumerado, daria error (creo) si lo paso como string
+            ApiController.post("Stripe/paymentintent", payment).then(function (data) {
+                console.log(data);
             });
         });
     });
+
+    $("#packXideas").click(function () {
+        purchaseData.numIdeasToBuy = parseInt(document.getElementById("numIdeas").value, 10);
+        //purchaseData.discount = 100; // ya elegido arriba cuando se mostraba
+        purchaseData.validateNumber = 1011;
+        purchaseData.expirationDate = "2024-04-05";
+
+        ApiController.post("packages/buy", purchaseData).then(function (data) {
+            if(data){
+                alert("Pago realizado con exito.");
+                window.location.href = "/home"
+            }else{
+                alert("Error al hacer el pago.");
+            }
+        });
+    });
+
+    $("#pack20ideas").click(function () {
+        purchaseData.numIdeasToBuy = 20;
+        purchaseData.discount = 0;
+        purchaseData.validateNumber = 1415;
+        purchaseData.expirationDate = "2024-04-05";
+
+        ApiController.post("packages/buy", purchaseData).then(function (data) {
+            if(data){
+                alert("Pago realizado con exito.");
+                window.location.href = "/home"
+            }else{
+                alert("Error al hacer el pago.");
+            }
+        });
+    });
 });
-
-// SOLO PARA QE EL USUARIO VEA EL DESCUENTO Y EL PRECIO FINAL, EL PRECIO NO SE PASA A BACK SINO QUE SE CALCULA ALLI POR SEGURIDAD
-function getDiscount(){
-    let dto, total, pricePerIdea = 7, numIdeasToBuy;
-
-    numIdeasToBuy = document.getElementById("numIdeas").value;
-
-    // PARA EL RANGO DE IDEAS MEJOR HACER UNA PETICION GET QUE NOS DEVUELVA LOS DESCUENTOS
-    if(numIdeasToBuy >= 40 && numIdeasToBuy < 250) {
-        dto = 0.05;
-        document.getElementById("descuento-seleccion").innerHTML = "Descuento: 5%";
-    }
-    else if (numIdeasToBuy >= 250 && numIdeasToBuy < 2000) {
-        dto = 0.1;
-        document.getElementById("descuento-seleccion").innerHTML = "Descuento: 10%";
-    }
-    else if (numIdeasToBuy >= 2000 && numIdeasToBuy < 3500) {
-        dto = 0.15;
-        document.getElementById("descuento-seleccion").innerHTML = "Descuento: 15%";
-    }
-    else{
-        dto = 0.2;
-        document.getElementById("descuento-seleccion").innerHTML = "Descuento: 20%";
-    }
-
-    total = pricePerIdea * numIdeasToBuy;
-    total -= total * dto;
-
-    document.getElementById("descuento-precio").innerHTML = total.toFixed(2) + " €";
-
-    console.log(total + " " + numIdeasToBuy);
-}
