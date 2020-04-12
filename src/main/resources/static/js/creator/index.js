8ca635834f555ebc201a587c2fc270065ccd82f2
csegundo <carlossn20165@gmail.com>
2020-04-12 18:52:10 +0200

    Arreglas unas cosas del home creator

--- src/main/resources/static/js/creator/index.js
+++ src/main/resources/static/js/creator/index.js
@@ -1,8 +1,6 @@
 $( document ).ready(function() {
 
-
     ApiController.get("auth/session", "", false).then(function (data) {
-        let listaIdeas = [];
         let id = data.id;
         let tamIdeas = data.idea.length;
         console.log(data);
@@ -11,84 +9,43 @@ $( document ).ready(function() {
         //Para listar todas las ideas
         for (let i = 0; i < tamIdeas; i++) {
             document.getElementById("myIdeas").innerHTML +=
-        '<div class="container lista-idea">\n'+
-                '<div class="col-12">\n'+
-                '<div class="row">\n'+
-                '<div class="col-6 text-left"><h3>'+data.idea[i].title+'</h3></div>\n'+
-            '<div class="col-6 col-lg-6 data text-right">\n'+
-                '<div class="row">\n'+
-
-                '<div class="col-10 data text-right">\n'+
-                '<span class="btn-edit">\n'+
-                '<a href="/creator/viewIdea"> <i class="fa fa-pencil-alt"style="font-size: 25px"></i></a>\n'+
-            '</span>\n'+
-            '</div>\n'+
-
-            '<div class="col-2 data text-right">\n'+
-                '<span class="btn-edit" onclick=confirm()>\n'+
-                '<a><i class="far fa-trash-alt" style="font-size: 25px"></i></a>\n'+
-            '</span>\n'+
-            '</div>\n'+
-            '</div>\n'+
-            '</div>\n'+
-            '</div>\n'+
-            '<div class="row">\n'+
-                '<div class="col-lg-12 text-right">\n'+
-                '<h5>'+data.idea[i].createdAt+'</h5></div>\n'+
-            '</div>\n'+
-            '<div class="row">\n'+
-                '<a href="/creator/viewIdea">' + data.idea[i].summary + '\n'+
-                '</a>\n'+
-            '</div>\n'+
-            '</div>\n'+
+            '<div class="container lista-idea">\n'+
+            '\t<div class="col-12">\n'+
+            '\t\t<div class="row">\n'+
+            '\t\t\t<div class="col-6 text-left">'+
+            '\t\t\t\t<h3>'+data.idea[i].title+'</h3>'+
+            '\t\t\t</div>\n'+
+            '\t\t\t<div class="col-6 col-lg-6 data text-right">\n'+
+            '\t\t\t\t<div class="row">\n'+
+            '\t\t\t\t\t<div class="col-10 data text-right">\n'+
+            '\t\t\t\t\t\t<span class="btn-edit">\n'+
+            '\t\t\t\t\t\t\t<a href="/creator/viewIdea"> <i class="fa fa-pencil-alt"style="font-size: 25px"></i></a>\n'+
+            '\t\t\t\t\t\t</span>\n'+
+            '\t\t\t\t\t</div>\n'+
+            '\t\t\t\t\t<div class="col-2 data text-right">\n'+
+            '\t\t\t\t\t\t<span class="btn-edit" onclick=confirm()>\n'+
+            '\t\t\t\t\t\t\t<a><i class="far fa-trash-alt" style="font-size: 25px"></i></a>\n'+
+            '\t\t\t\t\t\t</span>\n'+
+            '\t\t\t\t\t</div>\n'+
+            '\t\t\t\t</div>\n'+
+            '\t\t\t</div>\n'+
+            '\t\t</div>\n'+
+            '\t\t<div class="row">\n'+
+            '\t\t\t<div class="col-lg-12 text-right">\n'+
+            '\t\t\t\t<h5>'+data.idea[i].createdAt+'</h5>'+
+            '\t\t\t</div>\n'+
+            '\t\t</div>\n'+
+            '\t\t<div class="row">\n'+
+            '\t\t\t<a href="/creator/viewIdea">' + data.idea[i].summary + '</a>' + '\n'+
+            '\t\t</div>\n'+
+            '\t</div>\n'+
             '</div>\n ' +
-                '<div class="sep"></div>'
-
+            '<div class="sep"></div>'
         }
 
-        //Falta hacer las compradas (BACKEND)
+        // Ideas aceptadas: FALTA POR HACER
         for (let i = 0; i < tamIdeas; i++) {
-            document.getElementById("ideasCompradas").innerHTML +=
-                '<div class="container lista-idea">\n' +
-                '\t<div class="row">\n' +
-                '\t\t<div class="col-12 data">\n' +
-                '\t\t\t<a href="/enterprise/viewIdea/' + data.idea[i].id + '">\n' +
-                '\t\t\t\t<div class="row">\n' +
-                '\t\t\t\t\t<div class="col-8 col-lg-8 data">\n' +
-                '\t\t\t\t\t\t<strong>' + data.idea[i].title + '</strong>\n' +
-                '\t\t\t\t\t</div>\n' +
-                '\t\t\t\t\t<div class="col-4 col-lg-4 data text-right">\n' +
-                '\t\t\t\t\t\t<p>' + data.idea[i].createdAt + '\n' +
-                '\t\t\t\t\t\t\t<br>\n' +
-                '\t\t\t\t\t\t</p>\n' +
-                '\t\t\t\t\t\t<div class="row">\n' +
-                '\t\t\t\t\t\t\t<div class="col-3 data"></div>\n' +
-                '\t\t\t\t\t\t\t<div class="col-3 data"></div>\n' +
-                '\t\t\t\t\t\t</div>\n' +
-                '\t\t\t\t\t</div>\n' +
-                '\t\t\t\t</div>\n' +
-                '\n' +
-                '\n' +
-                '\t\t\t\t<div class="col-12 col-lg-12 data">\n' +
-                '\t\t\t\t\t<p>' + data.idea[i].summary + '</p>\n' +
-                '\t\t\t\t</div>\n' +
-                '\t\t\t</a>\n' +
-                '\t\t</div>\n' +
-                '\t</div>\n' +
-                '</div>\n'
         }
-
-
-
-        //Categorias disponibles
-        ApiController.get("categories/", "", false).then(function (category) {
-            //console.log(category);
-            for(let i = 0; i < category.length; i++)
-            {
-                document.getElementById("selectType").innerHTML +=
-                    '                <option value="Categoria '+ i +'">' + category[i].name + '</option>\n';
-            }
-        });
     });
 });
 
