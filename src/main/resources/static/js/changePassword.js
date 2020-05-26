cdb1eea2a5ae2368204922560848a2605901a15e
dani_ <daalca01@ucm.es>
2020-05-26 19:01:45 +0200

    Recuperación de Contraseña.

--- src/main/resources/static/js/changePassword.js
+++ src/main/resources/static/js/changePassword.js
@@ -3,15 +3,17 @@ function changePassword() {
     var repeatNewPassword;
     params.newPassword = document.getElementById("newPassword").value;
     repeatNewPassword = document.getElementById("confirmPassword").value;
-    params.token = location.search;
+    params.token =  location.search.substr(3);
     params.active = true;
     if(params.newPassword == repeatNewPassword){
-            ApiController.post("auth/updatePassword",params).then(function(data){
+            ApiController.put("auth/updatePassword",params, true).then(function(done){
                 if(done){
                     alert("Contraseña cambiada con exito");
                 }else{
                     alert("No se pudo cambiar la contraseña");
                 }
+
+                window.location.href = "/home";
             });
     }
 }
